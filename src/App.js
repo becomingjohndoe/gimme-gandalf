cb079cf996c95bd0e8192c06a8fa50da9c74a9fc
becomingjohndoe <karl.swatman@gmail.com>
2022-03-24 20:12:48 +0000

    set up App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Content from "./components/Content";
+import Form from "./components/Form";
+import Hero from "./components/Hero";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App mx-auto ">
+			<Hero />
+			<Form />
+		</div>
+	);
 }
 
 export default App;
