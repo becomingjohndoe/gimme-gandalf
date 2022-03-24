fb3c573937fdc1a900471c8ea87a33ef378bda85
becomingjohndoe <karl.swatman@gmail.com>
2022-03-24 20:13:22 +0000

    add Content component

---
+++ src/components/Content.jsx
@@ -0,0 +1,39 @@
+import lines from "../constants/lines.jsx";
+
+function updateClipboard(newClip) {
+	navigator.clipboard.writeText(newClip).then(
+		function () {
+			/* clipboard successfully set */
+		},
+		function () {
+			/* clipboard write failed */
+		}
+	);
+}
+
+function Content({ paragraphs }) {
+	return (
+		<div className="flex flex-col justify-center text-left p-4 max-w-[600px] mx-auto">
+			<button
+				onClick={() => {
+					updateClipboard(
+						paragraphs.map((p) => {
+							return p + "\n" + "\n" + "\n";
+						})
+					).join("");
+				}}
+			>
+				Copy
+			</button>
+			{paragraphs.map((line, index) => {
+				return (
+					<p key={index} className="p-4 ">
+						{line}
+					</p>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Content;
